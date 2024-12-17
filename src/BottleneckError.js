027a831ea61b14d3cc5b0542d8249374df0223da
decaffeinate <sean.derrow@gmail.com>
2024-12-17 12:00:30 -0500

    decaffeinate: Run post-processing cleanups on Batcher.coffee and 17 other files

--- src/BottleneckError.js
+++ src/BottleneckError.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 class BottleneckError extends Error {}
 
 module.exports = BottleneckError;
