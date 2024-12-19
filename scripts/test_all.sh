2822f390c9c8e186c62cfb9cece2077f5b82b17e
Sean Derrow <sean.derrow@gmail.com>
2024-12-19 06:24:54 -0500

    Clean up build

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -10,11 +10,5 @@ DATASTORE=ioredis npm test
 echo 'NodeRedis tests'
 DATASTORE=redis npm test
 
-echo 'ES5 bundle tests'
-BUILD=es5 npm test
-
-echo 'Light bundle tests'
-BUILD=light npm test
-
 echo 'Local tests'
 npm test
